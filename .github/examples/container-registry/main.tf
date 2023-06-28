cd954adfd8eb6477f96e2b22deb76b3890de9f8a
dkool <dennis.kool@cloudnation.nl>
2023-06-28 12:03:30 +0200

    feat: add usage examples

---
+++ .github/examples/container-registry/main.tf
@@ -0,0 +1,53 @@
+provider "azurerm" {
+  features {}
+}
+
+module "rg" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-rg"
+
+  environment = var.environment
+
+  groups = {
+    demo = {
+      region = "westeurope"
+    }
+  }
+}
+
+module "registry" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-acr"
+
+  workload    = var.workload
+  environment = var.environment
+
+  registry = {
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+    sku           = "Premium"
+  }
+  depends_on = [module.rg]
+}
+
+module "aks" {
+  source = "../../"
+
+  workload    = var.workload
+  environment = var.environment
+
+  aks = {
+    location            = module.rg.groups.demo.location
+    resourcegroup       = module.rg.groups.demo.name
+    node_resource_group = "${module.rg.groups.demo.name}-node"
+    dns_prefix          = "demo"
+
+    registry = {
+      attach = true, role_assignment_scope = module.registry.acr.id
+    }
+
+    default_node_pool = {
+      vmsize     = "Standard_DS2_v2"
+      node_count = 1
+    }
+  }
+  depends_on = [module.global]
+}
---
+++ .github/examples/simple/main.tf
@@ -0,0 +1,70 @@
+provider "azurerm" {
+  features {}
+}
+
+module "rg" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-rg"
+
+  environment = var.environment
+
+  groups = {
+    demo = {
+      region = "westeurope"
+    }
+  }
+}
+
+module "kv" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-kv"
+
+  workload    = var.workload
+  environment = var.environment
+
+  vault = {
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+
+    secrets = {
+      tls_public_key = {
+        aks = {
+          algorithm = "RSA"
+          rsa_bits  = 2048
+        }
+      }
+    }
+
+    contacts = {
+      admin = {
+        email = "dummy@cloudnation.nl"
+      }
+    }
+  }
+  depends_on = [module.rg]
+}
+
+module "aks" {
+  source = "../../"
+
+  workload    = var.workload
+  environment = var.environment
+
+  aks = {
+    location            = module.rg.groups.demo.location
+    resourcegroup       = module.rg.groups.demo.name
+    node_resource_group = "${module.rg.groups.demo.name}-node"
+
+    default_node_pool = {
+      vmsize     = "Standard_DS2_v2"
+      zones      = [1, 2, 3]
+      node_count = 1
+    }
+
+    profile = {
+      linux = {
+        username = "nodeadmin"
+        ssh_key  = module.kv.tls_public_key.aks.value
+      }
+    }
+  }
+  depends_on = [module.rg]
+}
