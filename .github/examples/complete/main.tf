965046fff1965f9dd1bdd83463a0a026037b5ee8
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2023-06-28 12:04:04 +0200

    feat: add usage examples (#4)

---
+++ .github/examples/complete/main.tf
@@ -0,0 +1,124 @@
+provider "azurerm" {
+  features {}
+}
+
+module "rg" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-rg"
+
+  environment = var.environment
+
+  groups = {
+    demo = {
+      region = "westeurope"
+    }
+  }
+}
+
+module "analytics" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-law"
+
+  workload    = var.workload
+  environment = var.environment
+
+  law = {
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+    sku           = "PerGB2018"
+    retention     = 90
+  }
+  depends_on = [module.rg]
+}
+
+module "kv" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-kv"
+
+  workload    = var.workload
+  environment = var.environment
+
+  vault = {
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+
+    secrets = {
+      tls_public_key = {
+        aks = {
+          algorithm = "RSA"
+          rsa_bits  = 2048
+        }
+      }
+    }
+
+    contacts = {
+      admin = {
+        email = "dummy@cloudnation.nl"
+      }
+    }
+  }
+  depends_on = [module.rg]
+}
+
+module "aks" {
+  source = "../../"
+
+  workload    = var.workload
+  environment = var.environment
+
+  aks = {
+    location            = module.rg.groups.demo.location
+    resourcegroup       = module.rg.groups.demo.name
+    node_resource_group = "${module.rg.groups.demo.name}-node"
+
+    enable = {
+      public_access = true
+    }
+
+    default_node_pool = {
+      vmsize     = "Standard_DS2_v2"
+      zones      = [1, 2, 3]
+      node_count = 1
+    }
+
+    maintenance = {
+      allowed = {
+        w1 = {
+          day   = "Saturday"
+          hours = ["1", "6"]
+        }
+        w2 = {
+          day   = "Sunday"
+          hours = ["1"]
+        }
+      }
+    }
+
+    workspace = {
+      id = module.analytics.law.id
+      enable = {
+        oms_agent = true
+        defender  = true
+      }
+    }
+
+    profile = {
+      network = {
+        plugin            = "azure"
+        load_balancer_sku = "standard"
+        load_balancer = {
+          idle_timeout_in_minutes   = 30
+          managed_outbound_ip_count = 10
+        }
+      }
+      autoscaler = {
+        enable = {
+          keda         = true
+          vertical_pod = true
+        }
+        linux = {
+          username = "nodeadmin"
+          ssh_key  = module.kv.tls_public_key.aks.value
+        }
+      }
+    }
+  }
+  depends_on = [module.rg]
+}
---
+++ .github/examples/node-pools/main.tf
@@ -0,0 +1,89 @@
+provider "azurerm" {
+  features {}
+}
+
+module "rg" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-rg"
+
+  environment = var.environment
+
+  groups = {
+    demo = {
+      region = "westeurope"
+    }
+  }
+}
+
+module "aks" {
+  source = "../../"
+
+  workload    = var.workload
+  environment = var.environment
+
+  aks = {
+    location            = module.rg.groups.demo.location
+    resourcegroup       = module.rg.groups.demo.name
+    node_resource_group = "${module.rg.groups.demo.name}-node"
+    channel_upgrade     = "stable"
+    dns_prefix          = "aksdemo"
+
+    default_node_pool = {
+      node_count = 1
+      config = {
+        linux_os = {
+          sysctl_config = {
+            fs_nr_open       = "1048576"
+            vm_max_map_count = "242144"
+          }
+        }
+        kubelet = {
+          allowed_unsafe_sysctls = ["kernel.shm*", "kernel.msg*"]
+          container_log_max_line = 100
+          cpu_manager_policy     = "static"
+        }
+      }
+      vmsize           = "Standard_DS2_v2"
+      zones            = [1, 2, 3]
+      workload_runtime = "OCIContainer"
+    }
+
+    node_pools = {
+      pool1 = {
+        vmsize     = "Standard_DS2_v2"
+        node_count = 1
+        max_surge  = 50
+        config = {
+          linux_os = {
+            sysctl_config = {
+              fs_nr_open        = "1048576"
+              fs_aio_max_nr     = "1048576"
+              net_core_wmem_max = "1048576"
+              vm_max_map_count  = "242144"
+            }
+          }
+          kubelet = {
+            allowed_unsafe_sysctls = ["net.*.", "kernel.msg*"]
+            container_log_max_line = 100
+            cpu_manager_policy     = "static"
+          }
+        }
+        workload_runtime = "OCIContainer"
+      }
+      pool2 = {
+        vmsize     = "Standard_DS2_v2"
+        node_count = 1
+        max_surge  = 50
+        config = {
+          linux_os = {
+            sysctl_config = {
+              fs_nr_open    = "1048576"
+              fs_aio_max_nr = "1048576"
+            }
+          }
+        }
+      }
+    }
+  }
+  depends_on = [module.rg]
+}
+
