7ebb2eddf344da8c353d2f454bebeb8f61a1e9d3
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2023-09-08 09:49:25 +0200

    feat: add consistent naming (#21)

--- examples/node-pools/main.tf
+++ examples/node-pools/main.tf
@@ -2,14 +2,46 @@ provider "azurerm" {
   features {}
 }
 
+module "naming" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-naming"
+
+  suffix = ["demo", "dev"]
+}
+
 module "rg" {
   source = "github.com/cloudnationhq/az-cn-module-tf-rg"
 
-  environment = var.environment
-
   groups = {
     demo = {
+      name   = module.naming.resource_group.name
       region = "westeurope"
+    }
+  }
+}
+
+module "kv" {
+  source = "github.com/cloudnationhq/az-cn-module-tf-kv"
+
+  naming = local.naming
+
+  vault = {
+    name          = module.naming.key_vault.name_unique
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+
+    secrets = {
+      random_string = {
+        secret1 = {
+          length  = 24
+          special = false
+        }
+      }
+      tls_keys = {
+        aks = {
+          algorithm = "RSA"
+          rsa_bits  = 2048
+        }
+      }
     }
   }
 }
@@ -17,10 +49,8 @@ module "rg" {
 module "aks" {
   source = "../../"
 
-  workload    = var.workload
-  environment = var.environment
-
   aks = {
+    name                = module.naming.kubernetes_cluster.name_unique
     location            = module.rg.groups.demo.location
     resourcegroup       = module.rg.groups.demo.name
     node_resource_group = "${module.rg.groups.demo.name}-node"
@@ -29,6 +59,13 @@ module "aks" {
     default_node_pool = {
       node_count = 1
       vmsize     = "Standard_DS2_v2"
+    }
+
+    profile = {
+      linux = {
+        username = "nodeadmin"
+        ssh_key  = module.kv.tls_public_keys.aks.value
+      }
     }
 
     node_pools = {
